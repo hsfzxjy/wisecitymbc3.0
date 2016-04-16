e0c87d6d0dde32c5651c98248ae9058c4d0a7789
hsfzxjy <956357208@qq.com>
2016-04-16 22:29:16 +0800

    A simple demo for vuejs playing with webpack.

---
+++ front-end/entries/index.vue
@@ -0,0 +1,19 @@
+<template>
+    
+</template>
+
+<script>
+    import Vue from 'vue';
+    import myHeader from '../components/my-header.vue';
+    import myFooter from '../components/my-footer.vue';
+    import splitter from '../components/splitter.vue';
+    import content from '../components/content.vue';
+
+    new Vue({
+        el: '#div',
+        data: {
+            title: 'This is the title.'
+        },
+        components: { myHeader,myFooter, splitter, content }
+    })
+</script>
