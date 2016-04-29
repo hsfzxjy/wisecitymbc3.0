02514c29553b993be5ef3d597b1bdde954a5b258
hsfzxjy <956357208@qq.com>
2016-04-29 23:10:51 +0800

    Check & init default groups after server starting. Realized using app.ready.

--- accounts/apps.py
+++ accounts/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+    def ready(self):
+        from .init_groups import create_default_groups
+
+        create_default_groups()
