dd5600b07ffd4a608acb8d3bfc35a255a6700009
hsfzxjy <956357208@qq.com>
2016-04-29 22:35:36 +0800

    Contributed more fields to User.

---
+++ accounts/consts.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class UserType(Enum):
+
+    government = 1
+    company = 2
+    bureau = 3
+
+EXPORTS = ['UserType']
--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,11 @@ from enhancements.auth.models import AbstractUser
 class User(AbstractUser):
 
     nickname = models.CharField(max_length=255, unique=True)
+    data = models.ForeignKey('UserData', null=True, blank=True)
+
+
+class UserData(models.Model):
+    name = models.CharField(max_length=255)
+    industry = models.CharField(max_length=255)
+    sector = models.CharField(max_length=255)
+    description = models.TextField()
