cfc495923b5c408878b8f0f23a82a0bdc01dfdb8
hsfzxjy <956357208@qq.com>
2016-04-29 21:44:24 +0800

    Finished constants app. Tests passed.

--- enhancements/constants/apps.py
+++ enhancements/constants/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class ConstantsConfig(AppConfig):
     name = 'constants'
+
+    def ready(self):
+        from .core import generate_consts
+
+        generate_consts()
