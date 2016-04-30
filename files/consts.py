b2377ab5189962d9e93cba78553bfcdcde59e873
hsfzxjy <956357208@qq.com>
2016-04-30 21:26:54 +0800

    Finished files models.

---
+++ files/consts.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+from enhancements.constants.utils import annotate
+from enhancements.shortcuts import _
+
+
+class FileType(Enum):
+
+    video = 1
+    image = 2
+    file = 3
+
+FILE_TYPE_CHOICES = annotate({
+    FileType.video: _('video'),
+    FileType.image: _('image'),
+    FileType.file: _('file')
+})
--- files/models.py
+++ files/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from . import consts
+
+
+class File(models.Model):
+    file_name = models.CharField(max_length=4096)
+    storage_url = models.URLField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    mime_type = models.CharField(max_length=255)
+    file_type = models.IntegerField(choices=consts.FILE_TYPE_CHOICES)
