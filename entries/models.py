4be665f7b600a8430468f14ac5ec195a339a7c15
hsfzxjy <956357208@qq.com>
2016-04-30 21:37:02 +0800

    Finished entries models.

--- entries/models.py
+++ entries/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.contenttypes import fields as ct_fields
 
-# Create your models here.
+from django.conf import settings
+
+
+class Entry(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    target_id = models.PositiveIntegerField()
+    target_content_type = models.ForeignKey(
+        'contenttypes.ContentType')
+    target = ct_fields.GenericForeignKey(
+        'target_content_type',
+        'target_id',
+    )
+
+    created_time = models.DateTimeField(auto_now_add=True)
+    type = models.IntegerField()
