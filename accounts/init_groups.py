45d8ea00b027abd76c619a48e960681193c669f0
hsfzxjy <956357208@qq.com>
2016-04-30 20:08:00 +0800

    Change default group names to English.

--- accounts/init_groups.py
+++ accounts/init_groups.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.models import Group
 
-from enhancements.shortcuts import _
-
 
 def create_default_groups():
-    Group.objects.get_or_create(name=_('goverment'))
-    Group.objects.get_or_create(name=_('company'))
-    Group.objects.get_or_create(name=_('bureau'))
+    from .consts import UserType
+
+    for item in UserType:
+        Group.objects.get_or_create(name=item.name)
