d987eb9b75a2fda94d46de083d3eab011bb6eb3f
hsfzxjy <956357208@qq.com>
2016-04-30 21:16:19 +0800

    Init app notifications.

---
+++ notifications/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.contenttypes import fields as contenttypes_fields
+
+from jsonfield import JSONField
+
+from django.conf import settings
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    template = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    data = JSONField()
+    has_read = models.BooleanField(default=False)
+    url = models.URLField()
+
+    target_id = models.PositiveIntegerField()
+    target_content_type = models.ForeignKey('contenttypes.ContentType')
+    target = contenttypes_fields.GenericForeignKey(
+        'target_content_type',
+        'target_id'
+    )
