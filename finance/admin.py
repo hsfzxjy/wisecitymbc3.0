7c3a3a7cae16822579b9f6fc850ed9ce1d4fbc8e
hsfzxjy <956357208@qq.com>
2016-05-01 21:11:32 +0800

    Finished undo actions admin integration.

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Stock, Bond, Futures, RawMaterials
+
+from django_object_actions import DjangoObjectActions
+
+from enhancements.shortcuts import _
+
+
+class BaseAdmin(DjangoObjectActions, admin.ModelAdmin):
+
+    def undo(self, request, obj):
+        obj.undo()
+
+    undo.label = _('Undo')
+    undo.short_description = _('Revert this object to previous version.')
+
+    change_actions = ('undo', )
+
+admin.site.register(Stock, BaseAdmin)
+admin.site.register(Bond, BaseAdmin)
+admin.site.register(Futures, BaseAdmin)
+admin.site.register(RawMaterials, BaseAdmin)
