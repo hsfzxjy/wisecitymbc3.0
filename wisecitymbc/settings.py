6621419bf6d23d878877718c91cfa5ee9b9a4f51
hsfzxjy <956357208@qq.com>
2016-06-11 23:34:47 +0800

    Add settings `REST_FRAMEWORK`.

--- wisecitymbc/settings.py
+++ wisecitymbc/settings.py
@@ -187,3 +187,13 @@ WEBPACK_LOADER = {
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = ['--nocapture',
              '--nologcapture', ]
+
+# rest_framework
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
+}
