d7fa5fc95ce56405388169759fc5fa6d6b792f71
hsfzxjy <956357208@qq.com>
2016-06-12 22:24:15 +0800

    Change rest_framework settings.

--- wisecitymbc/settings.py
+++ wisecitymbc/settings.py
@@ -195,5 +195,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
+    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+    'PAGE_SIZE': 10,
 }
