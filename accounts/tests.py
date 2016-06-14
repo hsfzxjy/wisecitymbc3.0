eea597e49b2527203a3e914f49c844ec0f3556db
hsfzxjy <956357208@qq.com>
2016-06-14 18:44:46 +0800

    Rename `create_test_users` to `create_users`.
    Since django testing backend searches for all
    members containing `test`, the original vari-
    able name would be mistaken for a test.

--- accounts/tests.py
+++ accounts/tests.py
@@ -6,6 +6,22 @@ from .models import User
 from .consts import UserType, BureauType
 
 
+def create_users():
+    return User.objects.create_user(
+        # id=1,
+        username='company',
+        nickname='company',
+        password='company',
+        user_type=UserType.company,
+    ), User.objects.create_user(
+        # id=2,
+        username='gov',
+        nickname='gov',
+        password='goverment',
+        user_type=UserType.government
+    )
+
+
 class UserTestCase(TestCase):
 
     INSTALLED_APPS = [
