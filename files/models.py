da9593d5e8764701e851f350b0ffd6b73b804573
hsfzxjy <956357208@qq.com>
2016-06-16 19:11:51 +0800

    Add verbose names.

--- files/models.py
+++ files/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
 from enhancements.models.fields import EnumField
+from enhancements.shortcuts import _
 
 from . import consts
 
 
 class File(models.Model):
-    file_name = models.CharField(max_length=4096)
-    storage_url = models.URLField()
-    created_time = models.DateTimeField(auto_now_add=True)
-    mime_type = models.CharField(max_length=255)
-    file_type = EnumField(consts.FileType, default=consts.FileType.file)
+    file_name = models.CharField(_('file name'), max_length=4096)
+    storage_url = models.URLField(_('url'))
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    mime_type = models.CharField(_('mime type'), max_length=255)
+    file_type = EnumField(consts.FileType,
+                          verbose_name=_('file type'),
+                          default=consts.FileType.file)
