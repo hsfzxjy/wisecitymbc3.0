8545bcdf38f2fbe4a04521deaf06cdafee34cd99
hsfzxjy <956357208@qq.com>
2016-06-17 16:05:17 +0800

    Fix search backends.

--- enhancements/rest/viewsets.py
+++ enhancements/rest/viewsets.py
@@ -1,14 +1,37 @@
 from enhancements.rest import registry
 
 from rest_framework import filters
+from rest_framework.response import Response
 
 
 class EnhancedViewSetMixin(object):
 
-    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    filter_backends = (filters.DjangoFilterBackend, filters.OrderingFilter)
     ordering_fields = ('id',)
     ordering = 'id'
     search_fields = ()
+
+    def render_list(self, queryset=None, filter=False):
+        if queryset is None:
+            queryset = self.get_queryset
+
+        if filter:
+            queryset = self.filter_queryset(queryset)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def render_object(self, obj=None):
+        if obj is None:
+            obj = self.get_object()
+
+        serializer = self.get_serializer(obj)
+        return Response(serializer.data)
 
     def get_serializer_class(self):
         model = self.get_queryset().model
