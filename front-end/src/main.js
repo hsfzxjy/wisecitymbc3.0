36496e86df5528ef0bf1878caa7d91e7af5e89d4
hsfzxjy <hsfzxjy@163.com>
2016-09-30 20:08:43 +0800

    temp

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+import Hello from './components/Hello'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+Vue.use(VueRouter)
+
+const routes = [
+    { path: '/hello', component: Hello }
+]
+
+let router = new VueRouter({
+    routes,
+    scrollBehavior: () => ({ y: 0 })
 })
+
+const app = new Vue({
+    el: 'app',
+    router,
+    ...App
+})
+
+console.log(app)
+
+export { app }
