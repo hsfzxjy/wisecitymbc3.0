5eb269f0721ba00819069fec5734f79dafae157e
hsfzxjy <956357208@qq.com>
2016-10-06 20:09:01 +0800

    Migrate to vue-cli 2.0.

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,25 +1,10 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import App from './App'
-import Hello from './components/Hello'
 
-Vue.use(VueRouter)
-
-const routes = [
-    { path: '/hello', component: Hello }
-]
-
-let router = new VueRouter({
-    routes,
-    scrollBehavior: () => ({ y: 0 })
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
 })
-
-const app = new Vue({
-    el: 'app',
-    router,
-    ...App
-})
-
-console.log(app)
-
-export { app }
